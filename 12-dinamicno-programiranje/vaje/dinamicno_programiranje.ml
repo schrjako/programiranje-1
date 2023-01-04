462f32297e65bce373d87eb20ac23224c9c4ed00
Jakob Schrader <jakob.schrader@guest.arnes.si>
2023-01-04 12:15:42 +0100

    Vaje dp solved

--- 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
+++ 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
@@ -21,6 +21,18 @@ let test_matrix =
      [| 2 ; 4 ; 5 |];
      [| 7 ; 0 ; 1 |] |]
 
+let max_cheese m =
+  let w = Array.length m.(0) and h = Array.length m in
+  let ansarr = Array.init h (fun _ -> Array.make w 0) in
+  for i = 0 to h - 1 do
+    for j = 0 to w - 1 do
+      if i <> 0 then ansarr.(i).(j) <- max ansarr.(i-1).(j) ansarr.(i).(j);
+      if j <> 0 then ansarr.(i).(j) <- max ansarr.(i).(j-1) ansarr.(i).(j);
+      ansarr.(i).(j) <- ansarr.(i).(j) + m.(i).(j)
+    done
+  done;
+  ansarr.(h-1).(w-1)
+
 (*----------------------------------------------------------------------------*]
  Poleg količine sira, ki jo miška lahko poje, jo zanima tudi točna pot, ki naj
  jo ubere, da bo prišla do ustrezne pojedine.
@@ -37,6 +49,38 @@ let test_matrix =
 [*----------------------------------------------------------------------------*)
 
 type mouse_direction = Down | Right
+
+let optimal_path_bottom m =
+  let w = Array.length m.(0) and h = Array.length m in
+  let ansarr = Array.init h (fun _ -> Array.make w 0) in
+  let dirs = Array.init h (fun _ -> Array.make w Down) in
+  for i = 0 to h - 1 do
+    for j = 0 to w - 1 do
+      if i <> 0 && ansarr.(i-1).(j) > ansarr.(i).(j) then
+        ansarr.(i).(j) <- ansarr.(i-1).(j);
+      if j <> 0 && ansarr.(i).(j-1) > ansarr.(i).(j) then
+        begin
+          let () = ansarr.(i).(j) <- ansarr.(i).(j-1) in
+          dirs.(i).(j) <- Right
+        end;
+      ansarr.(i).(j) <- ansarr.(i).(j) + m.(i).(j)
+    done
+  done;
+  let rec aux acc x y =
+    if x = 0 && y = 0 then acc
+    else match dirs.(x).(y) with
+    | Down -> aux (Down :: acc) (x-1) y
+    | Right -> aux (Right :: acc) x (y-1)
+  in
+  aux [] (h-1) (w-1)
+
+let convert_path m p =
+  let w = Array.length m.(0) and h = Array.length m in
+  let x, y, ints = List.fold_left (fun (x, y, acc) dir -> match dir with
+    | Down -> (x+1, y, m.(x).(y) :: acc)
+    | Right -> (x, y+1, m.(x).(y) :: acc)
+    ) (0, 0, []) p in
+  m.(h-1).(w-1) :: ints |> List.rev
 
 
 (*----------------------------------------------------------------------------*]
@@ -55,6 +99,15 @@ type mouse_direction = Down | Right
  - : int = 35
 [*----------------------------------------------------------------------------*)
 
+let alternating_towers v =
+  let arr = Array.init 2 (fun _ -> Array.init (v+1) (fun i -> if i = 0 then 1 else 0)) in
+  for i = 1 to v do
+    if i >= 2 then arr.(0).(i) <- arr.(1).(i-2);
+    if i >= 3 then arr.(0).(i) <- arr.(0).(i) + arr.(1).(i-3);
+    if i >= 1 then arr.(1).(i) <- arr.(0).(i-1);
+    if i >= 2 then arr.(1).(i) <- arr.(1).(i) + arr.(0).(i-2)
+  done;
+  arr.(0).(v) + arr.(1).(v)
 
 
 (*----------------------------------------------------------------------------*]
@@ -87,6 +140,26 @@ and blue_tower = TopBlue of blue_block * red_tower | BlueBottom
 
 type tower = Red of red_tower | Blue of blue_tower
 
+let enumerate_towers v =
+  let barr = Array.init (v+1) (fun i -> if i = 0 then [BlueBottom] else [])
+  and rarr = Array.init (v+1) (fun i -> if i = 0 then [RedBottom] else []) in
+  for i = 1 to v do
+    if i - 1 >= 0 then rarr.(i) <-
+      List.map (fun bt -> TopRed (Red1, bt)) barr.(i-1) |>
+      List.rev_append rarr.(i);
+    if i - 2 >= 0 then rarr.(i) <-
+      List.map (fun bt -> TopRed (Red2, bt)) barr.(i-2) |>
+      List.rev_append rarr.(i);
+    if i - 2 >= 0 then barr.(i) <-
+      List.map (fun rt -> TopBlue (Blue2, rt)) rarr.(i-2) |>
+      List.rev_append barr.(i);
+    if i - 3 >= 0 then barr.(i) <-
+      List.map (fun rt -> TopBlue (Blue3, rt)) rarr.(i-3) |>
+      List.rev_append barr.(i);
+  done;
+  List.rev_append (List.map (fun bt -> Blue bt) barr.(v))
+      (List.map (fun rt -> Red rt) rarr.(v))
+
 (*----------------------------------------------------------------------------*]
  Vdrli ste v tovarno čokolade in sedaj stojite pred stalažo kjer so ena ob
  drugi naložene najboljše slaščice. Želite si pojesti čim več sladkorja, a
@@ -108,3 +181,12 @@ type tower = Red of red_tower | Blue of blue_tower
 [*----------------------------------------------------------------------------*)
 
 let test_shelf = [1;2;-5;3;7;19;-30;1;0]
+
+let ham_ham shelf k =
+  let shelf = Array.of_list shelf in
+  let dp = Array.copy shelf in
+  for i = 0 to Array.length shelf - 1 do
+    if i != 0 then dp.(i) <- max dp.(i) dp.(i-1);
+    if i + k + 1 < Array.length shelf - 1 then dp.(i+k+1) <- dp.(i) + shelf.(i+k+1)
+  done;
+  dp.(Array.length dp - 1)
